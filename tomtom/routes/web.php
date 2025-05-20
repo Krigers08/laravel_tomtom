bf3c0cab170d3be8cc8636ffd171e276406602bf
Vigo28 <gamingvigo06@gmail.com>
2025-05-20 08:39:54 +0300

    .

--- tomtom/routes/web.php
+++ tomtom/routes/web.php
@@ -5,6 +5,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/notes', function () {
+Route::get('/index', function () {
     return view('notes');
 });
